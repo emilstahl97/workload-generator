df550d8ea9c0bbbf49bd47bc75f1c6c863a95dec
emilstahl97 <emil.stahl@icloud.com>
2023-11-23 15:19:09 +0100

    read file from cli args

--- traces/parse_trace.py
+++ traces/parse_trace.py
@@ -1,4 +1,5 @@
 import json
+import sys
 
 def parse_txt_to_json(file_path):
     data = {}
@@ -17,7 +18,7 @@ def parse_txt_to_json(file_path):
 
             for _ in range(num_flows):
                 flow_info = coflow_info[5 + _ * 3: 8 + _ * 3]
-                flow_source_id, flow_dest_id, flow_size = map(int, flow_info)
+                flow_source_id, flow_dest_id, flow_size = map(float, flow_info)
                 flows.append({'source_id': flow_source_id, 'dest_id': flow_dest_id, 'size_mb': flow_size})
 
             coflow_data = {
@@ -33,7 +34,20 @@ def parse_txt_to_json(file_path):
 
     return json.dumps(data, indent=2)
 
-# Example usage:
-file_path = '2000-0.9-FB-UP.txt'
-parsed_data = parse_txt_to_json(file_path)
-print(parsed_data)
+if __name__ == "__main__":
+    # Check if the file path is provided as a command line argument
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <file_path>")
+        sys.exit(1)
+
+    # Get the file path from the command line argument
+    file_path = sys.argv[1]
+
+    # Call the function with the provided file path
+    parsed_data = parse_txt_to_json(file_path)
+
+    # Save the parsed data to a json file
+    with open(file_path.replace('.txt', '.json'), 'w') as file:
+        file.write(parsed_data)
+
+    print(f"Data successfully parsed from {file_path} and saved to {file_path.replace('.txt', '.json')}")
