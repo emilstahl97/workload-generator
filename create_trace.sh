e0f5bc8b7f886d0f964936843a2e1e0751f6fa28
emilstahl97 <emil.stahl@icloud.com>
2023-11-23 17:44:57 +0100

    seems to work

--- create_trace.sh
+++ create_trace.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Run the trace producer
-trace_file_path=$(python3 ~/workload-generator/trace_producer.py 2000 FB-UP 0.9)
+trace_file_path=$(python3 ~/workload-generator/trace_producer.py 10 FB-UP 0.9)
 
 echo "Trace file path: $trace_file_path"
 
@@ -11,8 +11,17 @@ python3 ~/workload-generator/scripts/parse_trace.py "$trace_file_path"
 # Extract the name of the trace file without the path and extension
 trace_file_name=$(basename "$trace_file_path")
 
+echo "Trace file name: $trace_file_name"
+
 # Save the name of the json file
 json_file_name="${trace_file_name%.*}.json"
 
+echo "Json file name: $json_file_name"
+
+# the path to the json file is ~/workload-generator/json_traces/$json_file_name
+json_file_path=~/workload-generator/json_traces/$json_file_name
+
+echo "Json file path: $json_file_path"
+
 # Run the create_pcap_file.py script with the name of the json file
-python3 ~/workload-generator/scripts/create_pcap_file.py "$json_file_name"
+python3 ~/workload-generator/scripts/create_pcap_file.py "$json_file_path"
